1baeec11cd32cde24c2b7080cef8743734bf2733
Unknown <ykchautai@unal.edu.co>
2017-11-08 21:40:35 -0500

    .

--- start2.sh
+++ start2.sh
@@ -1,7 +1,7 @@
 ./rancher-compose --project-name sa-api-gateway \
 --url http://192.168.99.100:8080/v1/projects/1a5 \
---access-key 430EEDB3987BB6ED0805 \
---secret-key 5PYZagMThf5ErVh2SEdAVzxTH3Wv1K1HwczMTYji \
+--access-key E627DC694F378B4D52A3 \
+--secret-key QP4a4NRNQzHT52SqA6mYoX1fctqj2sZ9bU2oz4g5 \
 -f docker-compose.yml \
 --verbose up \
 -d --force-upgrade \
